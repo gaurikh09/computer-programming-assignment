ec6aa72ab9d2fae5cf52f79b60165435892dba2f
gaurikh09 <gauri.khandelwal_cs.h24@gla.ac.in>
2024-11-25 23:39:25 +0530

    Create string_q19.py

---
+++ coding-assignment/string/string_q19.py
@@ -0,0 +1,13 @@
+# program to print odd length strings
+# Input string
+s = "This is a python language"
+
+# Split the string into words
+words = s.split()
+
+# Iterate over each word
+for word in words:
+    # Check if the length of the word is even
+    if len(word) % 2 == 0:
+        # Print the word if its length is even
+        print(word, end=" ")
