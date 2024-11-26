744ca226b36ee75328f3228a180a5e5fc234246a
gaurikh09 <gauri.khandelwal_cs.h24@gla.ac.in>
2024-11-26 20:20:24 +0530

    Create list_q8.py

---
+++ coding-assignment/list/list_q8.py
@@ -0,0 +1,8 @@
+# check if an element is present or not
+a = [10, 20, 30, 40, 50]
+
+# Check if 30 exists in the list
+if 30 in a:
+    print("Element exists in the list")
+else:
+    print("Element does not exist")
