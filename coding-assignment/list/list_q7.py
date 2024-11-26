969d36c614ecea21195163362deec84ddbe9761c
gaurikh09 <gauri.khandelwal_cs.h24@gla.ac.in>
2024-11-26 20:19:29 +0530

    Create list_q7.py

---
+++ coding-assignment/list/list_q7.py
@@ -0,0 +1,13 @@
+# program to print double of a list
+a = [1, 2, 3, 4, 5]
+
+# Create an empty list 'res' to store results
+res = []
+
+# Iterate over each element in list 'a'
+for val in a:
+  
+    # Multiply each element by 2 and append it to 'res'
+    res.append(val * 2)
+
+print(res)
