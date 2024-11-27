985db30e073130e51f6be055ec1174eb9305f859
gaurikh09 <gauri.khandelwal_cs.h24@gla.ac.in>
2024-11-27 23:36:42 +0530

    Create function_q19.py

---
+++ coding-assignment/function/function_q19.py
@@ -0,0 +1,19 @@
+# merge list 
+def merge_sorted_lists(list1, list2):
+    result = []
+    i, j = 0, 0
+    
+    while i < len(list1) and j < len(list2):
+        if list1[i] < list2[j]:
+            result.append(list1[i])
+            i += 1
+        else:
+            result.append(list2[j])
+            j += 1
+    result.extend(list1[i:])
+    result.extend(list2[j:])
+    
+    return result
+list1 = [1, 3, 5, 7]
+list2 = [2, 4, 6, 8]
+print(merge_sorted_lists(list1, list2))  
