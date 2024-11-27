23d9bdb801563d5e35f433c14d3c5e221fbf21cc
gaurikh09 <gauri.khandelwal_cs.h24@gla.ac.in>
2024-11-27 22:57:30 +0530

    Create tuple_q3.py

---
+++ coding-assignment/tuple/tuple_q3.py
@@ -0,0 +1,5 @@
+# Create a tuple from user input and calculate its length.
+
+values = tuple(input("Enter tuple elements separated by spaces: ").split())
+length = len(values)
+print("Length of the tuple is:", length)
