4f316ae94db36646dea09c85a77b0e0ec4eedd63
gaurikh09 <gauri.khandelwal_cs.h24@gla.ac.in>
2024-11-27 22:43:05 +0530

    Create dict_q3.py

---
+++ coding-assignment/dict/dict_q3.py
@@ -0,0 +1,13 @@
+# acces values of a dicitionary
+# Create a dictionary
+person = {
+    "name": "Alice",
+    "age": 25,
+    "city": "New York"
+}
+
+# Access a value using a key
+age = person["age"]
+
+# Print the value
+print(age)  # Output: 25
