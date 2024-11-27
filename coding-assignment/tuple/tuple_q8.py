eebaab8abba35c68cadb6d8e0b444af85a576e13
gaurikh09 <gauri.khandelwal_cs.h24@gla.ac.in>
2024-11-27 23:03:22 +0530

    Create tuple_q8.py

---
+++ coding-assignment/tuple/tuple_q8.py
@@ -0,0 +1,11 @@
+# silced tu[ple
+t = (7, 14, 21, 28, 35)
+first_three = t[:3]
+last_three = t[-3:]
+reversed_tuple = t[::-1]
+length_of_tuple = len(t)
+
+print(first_three)
+print(last_three)
+print(reversed_tuple)
+print(length_of_tuple)
