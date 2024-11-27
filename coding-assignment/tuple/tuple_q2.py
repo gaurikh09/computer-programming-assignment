0c8a2954088d006216eaac0c3a2a4655af98f0da
gaurikh09 <gauri.khandelwal_cs.h24@gla.ac.in>
2024-11-27 22:57:05 +0530

    Create tuple_q2.py

---
+++ coding-assignment/tuple/tuple_q2.py
@@ -0,0 +1,5 @@
+#  Create a tuple and reverse its order.
+
+values = tuple(input("Enter tuple elements separated by spaces: ").split())
+reversed_tuple = values[::-1]
+print("Reversed Tuple:", reversed_tuple)
